675c2f126862eada3d1b3370db010d641873118d
as821 <andrew.r.stange@gmail.com>
2025-04-10 20:22:17 -0700

    forgot one

--- scripts/inspect_embed.py
+++ scripts/inspect_embed.py
@@ -11,15 +11,19 @@ sys.path.append(os.getcwd())
 sys.path.append(os.path.join(os.getcwd(), 'src'))
 sys.path.append(os.path.join(os.getcwd(), 'src/c'))
 
-from util import load_ckpt
+from util import load_ckpt, get_ckpt_path
 from preprocessor import generate_dset
+from matrix_utils import visualize_matrix, visualize_histogram
 
 
 import pdb
 
 
 def main(a):
-    args, sc_layer, smt_layer, whiten_op, unwhiten_op = load_ckpt(a.path, dense=True)
+    args, sc_layer, smt_layer, whiten_op, unwhiten_op = load_ckpt(a.path)
+    
+    args.debug_vis = a.debug_vis
+    
     dset = generate_dset(args, whiten_op=whiten_op, unwhiten_op=unwhiten_op)
     if a.test_set:
         dset = generate_dset(args, 'test', dset)
@@ -106,51 +110,41 @@ def main(a):
         n_patch_per_dict = torch.zeros(args.dict_sz)
 
         n_samples = 50
-
         embed = torch.zeros(n_samples, dset.n_patch_per_img, args.embed_dim)
         labels = torch.zeros(n_samples)
         for idx in tqdm(range(n_samples)):
             img, label = dset.get_single_eval_image(idx, a.stride)
             labels[idx] = label
-            img = img.unsqueeze(0).to("cuda", non_blocking=True)
-            sc = sc_layer(args, img, test=True, dense=True)
-            embed[idx] = smt_layer(sc, dense=True).permute((0, 2, 1)).cpu()
-
-            # stats on number of dictionary members for the patch
-            # n_dict_per_patch = sc.sum(dim=0)
-
-            # stats on which dictionary elements are the most/least frequent
-            n_patch_per_dict += sc[0].sum(dim=1).cpu()
-
-            # # stats on reconstruction error from sparse coding
-            # recon = sc_layer.basis @ (sc / sc.sum(dim=0))       # normalized by number of dictionary elements that correspond to a patch to roughly preserve magnitude     
-            # recon_err = (recon - img).abs()
+            img = img.to("cuda", non_blocking=True)
+            sc = sc_layer(img)
+            embed[idx] = smt_layer(sc).T.unsqueeze(0).cpu()
             
-
-
+            # stats on which dictionary elements are the most/least frequent
+            n_patch_per_dict += sc.sum(dim=1).cpu()
+        
         # stats on embedding cosine similarity --> embeddings already normalized
         embed = embed.flatten(0, 1)
         cos_sim = embed @ embed.T
+        assert cos_sim.max() < (1 + 1e-3), f"Cosine similarity < 1. Bug somewhere: {cos_sim.max()}"
         
-        cos_sim = einops.rearrange(cos_sim, "(a b) c -> a b c", a=n_samples)
-        
-        pdb.set_trace()
-        
-        indices = torch.ones_like(cos_sim, dtype=torch.bool)
-        indices.diagonal().fill_(False)     # remove diagonal elements
-        cos_sim_no_diag = cos_sim[indices]
-        
-        
+        visualize_matrix(args, cos_sim, "embed_cos_sim")
+        visualize_matrix(args, torch.cov(embed.T), "embed_dim_cov")
 
-        # print(f"\tDict el. per patch: (mean: {n_dict_per_patch.mean():.2f}, min: {n_dict_per_patch.min():.2f}, max: {n_dict_per_patch.max():.2f})")
-        n_patches = n_samples * dset.n_patch_per_img
-        print(f"\tPatch per dict. el.: (mean: {n_patch_per_dict.mean():.2f} ({n_patch_per_dict.mean()/n_patches:.2f}%), min: {n_patch_per_dict.min():.2f} ({n_patch_per_dict.min()/n_patches:.2f}%), max: {n_patch_per_dict.max():.2f} ({n_patch_per_dict.max()/n_patches:.2f}%))")
-        # print(f"\tRecon err per patch: (mean: {recon_err.mean():.4f}, min: {recon_err.min():.4f}, max: {recon_err.max():.4f})")
-        print(f"\tEmbedding cos. sim: (mean: {cos_sim_no_diag.mean():.4f}, min: {cos_sim_no_diag.min():.4f}, max: {cos_sim_no_diag.max():.4f})")
+        embed_hist = einops.rearrange(cos_sim, "(a b) (c d) -> a b c d", a=n_samples, c=n_samples)
 
+        # cosine sim to other patches in the same image (double counts due to cosine similarity symmetry and includes self-similarity)
+        intra_image_hist = embed_hist[torch.arange(n_samples), :, torch.arange(n_samples), :]
+        visualize_histogram(args, intra_image_hist.flatten(), "intra_image_sim")
 
+        # intra/inter class cosine similarity
+        intra_class, inter_class = [], []
+        for lab in torch.unique(labels):
+            mask = labels == lab
+            intra_class.append(embed_hist[mask][:, :, mask].flatten())
+            inter_class.append(embed_hist[mask][:, :, ~mask].flatten())
+        visualize_histogram(args, torch.concat(intra_class), "intra_class_sim")
+        visualize_histogram(args, torch.concat(inter_class), "inter_class_sim")
 
-        # TODO: also interesting to see embedding cosine sim across/within images and classes...
 
 
         # TODO: visualize some nearest neighbors in patch embedding space (within image, within class, within dataset)
@@ -161,13 +155,14 @@ def main(a):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', type=str, default="/home/astange/smt_ckpt")
-    parser.add_argument('--baseline', action="store_true")
-    parser.add_argument('--wandb', action="store_true")
     parser.add_argument('--test_set', action="store_true")
     parser.add_argument('--per_img', action="store_true")
 
     parser.add_argument('--stride', type=int, default=1)
     parser.add_argument('--context_sz', type=int, default=32)
+    
+    # parser.add_argument('--ckpt_path', type=str, default="/home/astange/smt_vis")
+    parser.add_argument('--debug_vis', action="store_true")
 
     with torch.no_grad():
         main(parser.parse_args())
